162f8dddc4cdf0e0f32f1d67306ad624047f3903
Jack <tfermin1827@hotmail.com>
2019-05-28 19:49:57 -0400

    Added signature recognition method

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const {secret} = require('../config');
 const router = Router();
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const crypto = require('crypto');
 
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'SourceBot'}); // Renders the index.pug file from the views folder
@@ -13,7 +14,7 @@ router.get('/maintenance', function (req, res, next) {
 });
 
 router.post('/deploy', function (req, res, next) {
-    if (req.headers['X-Hub-Signature'] === secret) {
+    if (req.headers['X-Hub-Signature'] === getSignature(secret)) {
 
         async function execute() {
             console.log(`Pulling changes from Github!`);
@@ -25,5 +26,11 @@ router.post('/deploy', function (req, res, next) {
         res.status(200).end();
     } else res.status(403).end();
 });
+
+function getSignature(buf) {
+    const hmac = crypto.createHmac("sha1", process.env.FB_APP_SECRET);
+    hmac.update(buf, "utf8");
+    return "sha1=" + hmac.digest("hex");
+}
 
 module.exports = router;
