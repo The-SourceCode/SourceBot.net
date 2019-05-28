26787c7624a08af26fa78c60a7618d36f459e190
Jack <tfermin1827@hotmail.com>
2019-05-28 19:40:14 -0400

    Auto deploy working

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const {Router} = require('express');
 const {secret} = require('../config');
 const router = Router();
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'SourceBot'}); // Renders the index.pug file from the views folder
@@ -11,9 +13,17 @@ router.get('/maintenance', function (req, res, next) {
 });
 
 router.post('/deploy', function (req, res, next) {
-    if (req.headers['X-Hub-Signature'] === secret) {}//TODO
-    console.log(req)
-    res.end(200);
+    if (req.headers['X-Hub-Signature'] === secret) {
+
+        async function execute() {
+            console.log(`Pulling changes from Github!`);
+            const commands = ["git fetch origin", "git pull origin testing"];
+            await exec('npm run pm2_restart');
+        }
+        execute().catch(console.error);
+
+        res.status(200).end();
+    } else res.status(403).end();
 });
 
 module.exports = router;
